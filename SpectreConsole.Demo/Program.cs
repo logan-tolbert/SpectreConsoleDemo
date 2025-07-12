effee2aa1a13378ebcd7883d65430c17209cde38
Logan <lodev205@gmail.com>
2025-07-12 08:23:51 -0500

    🎉 Initial commit
    - Create.editorconfig to define coding styles and conventions for C# and XML files.
    
    - Add .gitignore to exclude unnecessary files from version control.
    
    - Initialize Directory.Build.props and Directory.Packages.props for centralized package management.
    
    - Created solution file SpectreConsoleDemo.sln with two demo projects: SpectreConsole.Demo and SpectreConsoleDI.Demo.
    
    - Implemented basic logging functionality in both demo projects using Microsoft.Extensions.Logging and Spectre.Console.

---
+++ SpectreConsole.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Spectre.Console;
+
+// Create a logger factory and add console provider
+using var loggerFactory = LoggerFactory.Create(builder =>
+    builder.AddConsole());
+
+// Create the logger
+var logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("Application started\n");
+Console.WriteLine(Environment.NewLine);
+
+AnsiConsole.Markup("[underline red]Hello[/] World!");
+
+Console.WriteLine(Environment.NewLine);
+logger.LogInformation("Application finished");
---
+++ SpectreConsoleDI.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Spectre.Console.Cli.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+// Manual logging registration if AddLogging() isn't available
+services.AddSingleton<ILoggerFactory, LoggerFactory>();
+services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+services.AddSingleton<ILoggerProvider, ConsoleLoggerProvider>();
+
+services.AddLogging(builder =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+using var registrar = new DependencyInjectionRegistrar(services);
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
+{
+    // configure your commands
+});
+
+return await app.RunAsync(args);
